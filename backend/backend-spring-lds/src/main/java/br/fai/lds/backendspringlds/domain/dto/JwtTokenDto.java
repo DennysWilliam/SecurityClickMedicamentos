37ba186b95dfbe6472e98cb9b4aec06ff47bc68d
Dennys William <dennyswilliam018@gmail.com>
2025-12-02 14:50:41 -0300

    :sparkles: feat: criacao do JwtTokenDto

---
+++ backend/backend-spring-lds/src/main/java/br/fai/lds/backendspringlds/domain/dto/JwtTokenDto.java
@@ -0,0 +1,18 @@
+package br.fai.lds.backendspringlds.domain.dto;
+
+public class JwtTokenDto {
+
+    private String token;
+
+    public JwtTokenDto(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
