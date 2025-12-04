4812fe565f9026040cab8bd28e7b32e785e0effd
Ana <anajumendes08@gmail.com>
2025-12-04 11:19:14 -0300

    feat: alteracao do front

--- front-end/src/environments/environment.devolopment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    env: 'development',
-    api_endpoint: 'http://localhost:3000',
-    authentication_api_endpoint: 'http://localhost:3000'
-};
--- front-end/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    env: 'production',
-    api_endpoint: 'http://localhost:8080/api',
-    authentication_api_endpoint: 'http://localhost:8080'
-};
